44d0cfc6da1a322617f0197494e2f26989697cc6
Pooja <poojakadwadi8888@gmail.com>
2026-03-23 10:54:34 +0530

    Update +config.js

--- pages/contactus/+config.js
+++ pages/contactus/+config.js
@@ -1,4 +1,5 @@
 export default {
+  
   title: 'Contact Novara Nature Estates | Book Site Visit',
   metaDescription: 'Get in touch with Novara Nature Estates to book a farmland site visit  or speak with our experts about managed farmland investments.'
 }
