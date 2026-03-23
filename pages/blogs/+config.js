643d86267621b622609b09762812e681eb0b514f
Pooja <poojakadwadi8888@gmail.com>
2026-03-23 10:51:24 +0530

    Update +config.js

--- pages/blogs/+config.js
+++ pages/blogs/+config.js
@@ -1,4 +1,5 @@
 export default {
+  
   title: 'Farmland Investment Blogs | Novara Nature Estates',
   metaDescription: 'Explore expert insights on managed farmland investment, sustainability & agricultural land ownership near Bangalore from Novara Nature Estates.'
 }
